19e96f50e0d4fdebbfaece2d27819c80844a4723
Wenting Li <liwenting0601@gmail.com>
2017-09-04 18:57:50 -0400

    m

--- Matlab_code/readPMUdata.m
+++ Matlab_code/readPMUdata.m
@@ -1,14 +1,7 @@
-function [ allnames, len ] = read_all_file( path,current_path )
-    cd(path);
-    list=dir('*.mat');
-    name={list.name}; 
-    allnames=natsort(name);
-    [k,len]=size(allnames); 
-    cd(current_path);
+function [bus_v,bus_freq ,name,ilf]=readPMUdata(path,allnames,num_bus) 
+    name=fullfile(path,allnames{1,num_bus});
+    data=load(name);
+    bus_v= data.bus_v;
+    bus_freq=data.bus_freq;
+    bus_theta=data.theta;  
 end
-
-
-
-
- 
- 
